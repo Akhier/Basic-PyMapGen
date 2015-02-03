dbe2e3caade1a192de5dc084b67fe7f9a95823a7
akhier <akhierthedragonhearted@yahoo.com>
2015-02-03 02:06:18 -0500

    +Map

---
+++ Map.py
@@ -0,0 +1,38 @@
+from Rect import Rect
+
+class Map:
+    def __init__(self, h, w):
+        self.Height = h
+        self.Width = w
+        self._tiles = [[0 for y in range(self.Height)] for x in range(self.Width)]
+
+    def set_tile(self, id, x, y):
+        self._tiles[y][x] = id
+
+    def get_tile(self, x, y):
+        return self._tiles[y][x]
+
+    def set_area(self, id, r):
+        for y in  range(0, r.Height - 1):
+            for x in range(0, r.Width - 1):
+                self.set_tile(id, r.X + x, r.Y + y)
+
+    def set_edge(self, id, r):
+        for y in range(r.Y, r.Y + r.Height):
+            for x in range(r.X, r.X + r.Width):
+                if r.is_edge(x, y):
+                    self.set_tile(id, x, y)
+
+if __name__ == '__main__':
+    test = Map(9, 12)
+    x = 0
+    y = 1
+    test.set_tile(2, x, y)
+    print (test.get_tile(x, y))
+    x2 = 2
+    y2 = 3
+    h = 4
+    w = 5
+    test.set_area(3, Rect.create_new(x2, y2, h, w))
+    test.set_edge(4, Rect.create_new(x2, y2, h, w))
+    print (test._tiles)
